7d019da49f45001b889de2c496ff946e9633263c
Tiberiu Ion <tiberiu.vi@gmail.com>
2018-04-29 10:40:46 +0100

    267 yelpcamp adding mongoose

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express		 	= require("express"),
+	app		 		= express(),
+	bodyParser		= require("body-parser"),
+	mongoose		= require("mongoose");
+
+
+//connect mongoose
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+//campground object model
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//create an entry
+// Campground.create(
+// 	{
+// 		name:'Malvern Hills',
+// 		image:"http://unsplash.it/300/300"
+// 	}, function(err, campground){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("new campground created");
+// 			console.log(campground);
+// 		}
+// 	});
+	
+
 app.use(express.static(__dirname + "/www"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
-var campgrounds = [ 
-		{name:'John Smith', image:"http://unsplash.it/300/300"},
-		{name:'Annabel Kay', image:"http://unsplash.it/301/301"},
-		{name:'Amanda Palmer', image:"http://unsplash.it/299/299"},
-		{name:'Josh Hammersmith', image:"http://unsplash.it/301/302"},
-	];
 
 app.get("/", function(req, res){
 	res.render("home");
@@ -18,7 +41,15 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
 
-	res.render("campgrounds", {campgrounds: campgrounds });
+//get all campgrounds from the db and render file
+Campground.find({}, function(err, allCampgrounds){
+if(err){
+	console.log(err);
+} else {
+	res.render("campgrounds", {campgrounds: allCampgrounds });
+}
+});
+	// res.render("campgrounds", {campgrounds: campgrounds });
 });
 
 
@@ -28,10 +59,15 @@ app.post("/campgrounds", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	//redirect back to campgrounds page
-	res.redirect("/campgrounds");
-	
+	// create a new campground and save to db
+	Campground.create(newCampground, function(err, newlyCreated){
+		if (err) {
+			console.log(err);
+		}else {
+			//redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
 });
 
 

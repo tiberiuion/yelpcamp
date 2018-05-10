c54d0a6594cdcff192466081607ca889458f1a4a
Tiberiu Ion <T.Ion@ucas.ac.uk>
2018-05-10 12:23:39 +0100

    293 yelpcamp creating comments pt2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express		 	= require("express"),
 	bodyParser		= require("body-parser"),
 	mongoose		= require("mongoose"),
 	Campground 		= require("./models/campground"),
+	Comment 		= require("./models/comment"),
 	seedDB 			= require("./seeds");
 
 //connect mongoose
@@ -84,6 +85,28 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 	});
 });
 
+app.post("/campgrounds/:id/comments", function(req, res){
+	//lookup campground using ID
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			Comment.create(req.body.comment, function(err, comment){
+				if (err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
+		}
+	});
+	//create new comment
+	//connect new comment to campground
+	//redirect campground showpage
+});
 
 
 app.listen("3000", function(){

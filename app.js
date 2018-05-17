a07cebdda652acd3af6b9c886a26a4f10c25f5f7
Tiberiu Ion <T.Ion@ucas.ac.uk>
2018-05-17 09:25:44 +0100

    307 yelpcamp adding auth pt4

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get("/campgrounds/:id", function(req, res){
 // Comments routes
 //============================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn ,function(req, res){
 	//find campground by id
 	Campground.findById(req.params.id, function(err, campground){
 		if (err) {
@@ -99,7 +99,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 	});
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments",isLoggedIn ,function(req, res){
 	//lookup campground using ID
 	Campground.findById(req.params.id, function(err, campground){
 		if(err){
@@ -160,6 +160,20 @@ app.post("/login", passport.authenticate("local",
 	}), function (req, res) {
 
 });
+
+//add logout route
+app.get("/logout", function (req, res) {
+	//this comes from the packages we have installed
+	req.logout();
+	res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+};
 
 
 app.listen("3000", function(){

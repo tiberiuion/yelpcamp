b57aa81a11d671fff744641356e5a45aa8cb2a71
Tiberiu Ion <tiberiu.vi@gmail.com>
2018-05-20 20:59:47 +0100

    311 yelpcamp user association: campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,12 +19,17 @@ router.get("/", function(req, res){
 
 //CREATE
 //I really have no idea what's happening here
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
 	//get data from form and add to campgrounds array
 	var name = req.body.name;
 	var image = req.body.image;
 	var description = req.body.description;
-	var newCampground = {name: name, image: image, description: description};
+	var author = {
+		id: req.user._id,
+		username:req.user.username
+	};
+	var newCampground = {name: name, image: image, description: description, author: author};
+	console.log(req.user);
 	// create a new campground and save to db
 	Campground.create(newCampground, function(err, newlyCreated){
 		if (err) {
@@ -37,7 +42,7 @@ router.post("/", function(req, res){
 });
 
 //NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn,function(req, res){
 	res.render("campgrounds/new");
 });
 
@@ -54,6 +59,15 @@ router.get("/:id", function(req, res){
 		}
 	});
 });
+
+//middleware
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
+
 
 
 module.exports =  router;

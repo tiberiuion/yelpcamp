35e41ebf65183882a92afa9ca77789a2884f6954
Tiberiu Ion <T.Ion@ucas.ac.uk>
2018-05-21 08:56:34 +0100

    314 yelpcamp campground destroy

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -85,6 +85,18 @@ router.put("/:id", function(req, res){
 	//redirect to the show page
 });
 
+//DESTROY CAMPGROUND ROUTE
+router.delete("/:id", function (req,res) {
+	Campground.findByIdAndRemove(req.params.id, function(err){
+		if (err) {
+			res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+} );
+
+
 //middleware
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()){

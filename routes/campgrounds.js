2485867e7692ed20791138ef1cc4e8ce58c0c8bc
Tiberiu Ion <T.Ion@ucas.ac.uk>
2018-05-22 09:12:43 +0100

    authorization pt2

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -72,7 +72,7 @@ router.get("/:id/edit", checkCampgroundOwnership ,function(req, res){
 
 
 //UPDATE CAMPGROUND ROUTE
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
 	//find and update the correct campground
 	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
 		if (err) {
@@ -85,7 +85,7 @@ router.put("/:id", function(req, res){
 });
 
 //DESTROY CAMPGROUND ROUTE
-router.delete("/:id", function (req,res) {
+router.delete("/:id", checkCampgroundOwnership, function (req,res) {
 	Campground.findByIdAndRemove(req.params.id, function(err){
 		if (err) {
 			res.redirect("/campgrounds");

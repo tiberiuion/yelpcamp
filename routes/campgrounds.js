bfcd3d7f3cc774f73755173f85b3525b48afca53
Tiberiu Ion <T.Ion@ucas.ac.uk>
2018-05-22 08:25:47 +0100

    316 yelpcamp campground authorizations pt1

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -61,13 +61,12 @@ router.get("/:id", function(req, res){
 });
 
 //EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", function(req, res){
+router.get("/:id/edit", checkCampgroundOwnership ,function(req, res){
+	//is user logged in
 	Campground.findById(req.params.id, function(err, foundCampground){
-		if (err) {
-			res.redirect('/campgrounds');
-		} else {
-			res.render("campgrounds/edit", {campground: foundCampground});
-		}
+		res.render("campgrounds/edit", {campground: foundCampground});
+	// otherwise redirect
+	//if not redirect 
 	});
 });
 
@@ -103,6 +102,25 @@ function isLoggedIn(req, res, next) {
 		return next();
 	}
 	res.redirect("/login");
+};
+
+function checkCampgroundOwnership (req, res, next){
+	if(req.isAuthenticated()){
+		Campground.findById(req.params.id, function(err, foundCampground){
+			if (err) {
+				res.redirect('back');
+			} else {
+				//does user own camoground
+				if (foundCampground.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect("back");
+				}
+			}
+		});
+	} else {
+		res.redirect("back");
+	}
 }
 
 

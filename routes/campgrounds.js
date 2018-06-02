cff9f52e2e2cccb10fcf3945f28e4f87aeb4b0e5
Tiberiu Ion <tiberiu.vi@gmail.com>
2018-06-02 11:53:51 +0100

    330 yelpcamp dynamic price feature

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -23,11 +23,12 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
+    var price = req.body.price;
     var author = {
         id: req.user._id,
         username:req.user.username
     };
-    var newCampground = {name: name, image: image, description: description, author: author};
+    var newCampground = {name: name, image: image, description: description, author: author, price: price};
     // create a new campground and save to db
     Campground.create(newCampground, function(err, newlyCreated){
         if (err) {
